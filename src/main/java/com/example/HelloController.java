6a3ae14d830686d9ec3c7061512bfb7f79904240
Johan Briger <johanbriger@gmail.com>
2025-11-14 08:41:10 +0100

    Improvements from AI-Bot part 3

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -68,6 +68,8 @@ public class HelloController {
                             }
                         });
                         setGraphic(downloadLink);
+                    }else  {
+                        setGraphic(null);
                     }
 
 
